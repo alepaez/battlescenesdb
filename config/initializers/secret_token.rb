9fbd45f4776d38918b56f64fe271dd143bdec966
Alexandre Paez <alepaezseq@gmail.com>
2014-05-30 12:38:51 -0300

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battlescenesdb::Application.config.secret_token = '54bccc8f40dc5cb133ffc02524fe53e2e05111ede498f33c9cb743e407637dc5f5e8db008f4410234454a0b1825afbbffe06409df47ad28472fdf0dfd657837b'
