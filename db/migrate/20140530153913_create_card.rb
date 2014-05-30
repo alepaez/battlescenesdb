361515d35a913c2da57b55cb392a237618f679ed
Alexandre Paez <alepaezseq@gmail.com>
2014-05-30 14:02:05 -0300

    Cards

---
+++ db/migrate/20140530153913_create_card.rb
@@ -0,0 +1,37 @@
+class CreateCard < ActiveRecord::Migration
+  def up
+    create_table :cards, id: false do |t|
+      t.column :id, :integer, options: 'PRIMARY KEY'
+      t.column :rarity, :string
+      t.column :type, :string
+      t.column :faction, :string
+      t.column :alignment, :string
+      t.column :name, :string
+      t.column :alter_ego, :string
+      t.column :energy, :integer
+      t.column :shield, :integer
+
+      t.column :agility, :boolean
+      t.column :ranged_attack, :boolean
+      t.column :energy_attack, :boolean
+      t.column :elasticity, :boolean
+      t.column :genius, :boolean
+      t.column :blades, :boolean
+      t.column :magic, :boolean
+      t.column :regeneration, :boolean
+      t.column :super_strength, :boolean
+      t.column :telekinesis, :boolean
+      t.column :telepathy, :boolean
+      t.column :flying, :boolean
+
+      t.column :anticipation, :boolean
+      t.column :unforeseen, :boolean
+
+      t.column :text, :text
+    end
+  end
+
+  def down
+    drop_table :cards
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20140530153913) do
+
+  create_table "cards", :id => false, :force => true do |t|
+    t.integer "id"
+    t.string  "rarity"
+    t.string  "type"
+    t.string  "faction"
+    t.string  "alignment"
+    t.string  "name"
+    t.string  "alter_ego"
+    t.integer "energy"
+    t.integer "shield"
+    t.boolean "agility"
+    t.boolean "ranged_attack"
+    t.boolean "energy_attack"
+    t.boolean "elasticity"
+    t.boolean "genius"
+    t.boolean "blades"
+    t.boolean "magic"
+    t.boolean "regeneration"
+    t.boolean "super_strength"
+    t.boolean "telekinesis"
+    t.boolean "telepathy"
+    t.boolean "flying"
+    t.boolean "anticipation"
+    t.boolean "unforeseen"
+    t.text    "text"
+  end
+
+end
